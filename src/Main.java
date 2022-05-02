c3dc39978a43e9684a08d21d392a70df8715b13e
Maksim Androfagin <androfagin@mail.ru>
2022-05-02 22:03:29 +0400

    Create new project PatternFacade

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        BinOps bins = new BinOps();
+        System.out.println(bins.sum("1010", "100010"));
+        System.out.println(bins.mult("11010", "10001"));
+    }
+}
